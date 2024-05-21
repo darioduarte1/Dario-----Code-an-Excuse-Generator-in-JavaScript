e160534811a88fa72c80e1787c321c40c35cf7a4
darioduarte1 <darioduarte1991@gmail.com>
2024-05-21 13:57:39 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,35 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Función para generar la excusa
+function excuseGenerator() {
+  let numWho = Math.floor(Math.random() * who.length);
+  let numAction = Math.floor(Math.random() * action.length);
+  let numWhat = Math.floor(Math.random() * what.length);
+  let numWhen = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
+  return (
+    who[numWho] +
+    " " +
+    action[numAction] +
+    " " +
+    what[numWhat] +
+    " " +
+    when[numWhen]
+  );
+}
 
-  const whoRandom = who[Math.floor(math.random() * who.length)];
-  const actionRandom = action[Math.floor(math.random() * action.length)];
-  const whatRandom = what[Math.floor(math.random() * what.length)];
-  const whenRandom = when[Math.floor(math.random() * when.length)];
+// Función para asignar la excusa al elemento HTML
+function assignExcuse() {
+  const excuseElement = document.querySelector("#excuse");
+  excuseElement.innerHTML = excuseGenerator(); // Asignar la excusa generada
+}
 
-  excuseGenerada =
-    who[whoId] + " " + what[whatId] + " " + when[whenId] + " " + where[whereId];
-
-  return sum_message;
-
-  const excuse = excuseGenerada;
-  const mainParagraph = document.getElementById("excuse");
-  mainParagraph.innerHTML = excuse;
-
-  console.log("Hello Rigo from the console!");
-};
+// Asegurar que el código se ejecute después de que la página se cargue
+document.addEventListener("DOMContentLoaded", assignExcuse);
