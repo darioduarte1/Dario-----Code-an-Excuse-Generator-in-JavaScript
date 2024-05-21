cc9acc5290a47e3119052ec8beda49427422f262
darioduarte1 <darioduarte1991@gmail.com>
2024-05-21 12:52:29 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const whoRandom = who[Math.floor(math.random() * who.length)];
+  const actionRandom = action[Math.floor(math.random() * action.length)];
+  const whatRandom = what[Math.floor(math.random() * what.length)];
+  const whenRandom = when[Math.floor(math.random() * when.length)];
+
+  excuseGenerada =
+    who[whoId] + " " + what[whatId] + " " + when[whenId] + " " + where[whereId];
+
+  return sum_message;
+
+  const excuse = excuseGenerada;
+  const mainParagraph = document.getElementById("excuse");
+  mainParagraph.innerHTML = excuse;
+
   console.log("Hello Rigo from the console!");
 };
